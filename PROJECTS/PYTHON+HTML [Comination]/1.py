92f2ef921bb87f09dc4fbd4b996eab511eb4b067
HARSH D <148961735+harsh1d@users.noreply.github.com>
2024-11-02 19:23:44 +0530

    Add files via upload

---
+++ PROJECTS/PYTHON+HTML [Comination]/1.py
@@ -0,0 +1,20 @@
+A=5
+B=6
+C=A+B
+print("\nThe value of C is: ",C)
+
+# #Exercise 2
+# def add_numbers(a,b):
+#     return a+b
+# X = int(input("Enter the first  number to add = ",))
+# Y = int(input("Enter the second number to add = "))
+# Z = add_numbers(X,Y)
+# print("\n\n The sum of", X,"and", Y,"is = ", Z)
+
+# #Exercise 3
+# name = input ("Please enter your name : ")
+# age = input ("Please enter your age     : ")
+# gender = input ("Please enter your gender     : ")
+# print ("Name   = %s"%name )
+# print ("Age    = %s"%age  )
+# print ("Gender = %s"%gender)
